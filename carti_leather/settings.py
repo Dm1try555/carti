625e76aba13c1ac525a45c12e3393da380219138
Dima <shilovscky2020@gmail.com>
2025-06-22 08:29:32 +0300

    fix6

--- carti_leather/settings.py
+++ carti_leather/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'UA'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'Europe/Kiev'
 
